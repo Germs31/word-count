638868ff30ceaec333ae0204860afa0305b5b2aa
german nunez <german90nunez@gmail.com>
2021-10-13 18:23:53 -0500

    added comments to explain my thought process for this challenge

--- word-count.js
+++ word-count.js
@@ -4,10 +4,15 @@
 //
 
 export const countWords = (phrase) => {
+    // initalizing empty obj to return answer in
     let wordObj = {}
+    // making sure that pharse is lowercased and remove special characters accept valid ones. 
     let words = phrase.toLowerCase().match(/\w+('\w+)?/g)
+    // loop phrase as it was made into an array
     words.forEach((w) => {
+        // checks if words are a key in obj and if so add the word as a key and update amount of times it was spotted in phrase. 
         wordObj.hasOwnProperty(w) ? wordObj[w]++ : wordObj[w] = 1;
     })
+    // returns obj with words as key and count as value
     return wordObj
 };
